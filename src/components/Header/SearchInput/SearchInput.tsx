0f04be21734685c1762ab9670823dcabc2d091e7
Kareem Ashkar <kareemalachkar@gmail.com>
2023-11-22 13:12:07 +0300

    finished

--- src/components/Header/SearchInput/SearchInput.tsx
+++
@@ -1,9 +0,0 @@
-import './SearchInput.scss'
-
-export const SearchInput = () => {
-  return (
-    <div className="search-container">
-      <input type="text" placeholder="Search: Coats, sweaters, sweatshirts, trousers, bags..." />
-    </div>
-  )
-}
--- src/views/SignIn/SignIn.tsx
+++
@@ -1,9 +0,0 @@
-import './SignIn.scss'
-
-export const SignIn = () => {
-  return (
-    <>
-      <h3>Sign In</h3>
-    </>
-  )
-}
